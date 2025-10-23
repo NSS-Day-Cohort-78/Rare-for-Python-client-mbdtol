b28194eb376f6778e45ce75fc856b7dd2fcb2cb2
charlesmerriman <zacmerr@gmail.com>
2025-10-23 09:27:28 -0500

    Finalize merge

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,28 +1,29 @@
-import { Route, Routes } from "react-router-dom";
-import { Login } from "../components/auth/Login";
-import { Register } from "../components/auth/Register";
-import { Authorized } from "./Authorized";
-import { MyPosts } from "../components/myposts/MyPosts";
-import { EditPostForm } from "../components/myposts/EditPost";
-import { PostDetails } from "../components/myposts/PostDetails";
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../components/auth/Login"
+import { Register } from "../components/auth/Register"
+import { Authorized } from "./Authorized"
+import { MyPosts } from "../components/myposts/MyPosts"
+import { EditPostForm } from "../components/myposts/EditPost"
+import { PostDetails } from "../components/myposts/PostDetails"
+import { AllPosts } from "../components/myposts/AllPosts"
 
 export const ApplicationViews = ({ token, setToken }) => {
-  return (
-    <>
-      <Routes>
-        <Route path="/login" element={<Login setToken={setToken} />} />
-        <Route path="/register" element={<Register setToken={setToken} />} />
-        <Route element={<Authorized token={token} />}>
-          {/* Add Routes here */}
-        <Route path="/myposts" element={<MyPosts token={token} />} />
-        <Route path="/allposts" element={<AllPosts token={token} />} />
-        <Route path="/post-details" element={<PostDetails token={token} />} />
-          <Route
-            path="/edit-post/:postId"
-            element={<EditPostForm token={token} />}
-          />
-        </Route>
-      </Routes>
-    </>
-  );
-};
+	return (
+		<>
+			<Routes>
+				<Route path="/login" element={<Login setToken={setToken} />} />
+				<Route path="/register" element={<Register setToken={setToken} />} />
+				<Route element={<Authorized token={token} />}>
+					{/* Add Routes here */}
+					<Route path="/myposts" element={<MyPosts token={token} />} />
+					<Route path="/allposts" element={<AllPosts token={token} />} />
+					<Route path="/post-details" element={<PostDetails token={token} />} />
+					<Route
+						path="/edit-post/:postId"
+						element={<EditPostForm token={token} />}
+					/>
+				</Route>
+			</Routes>
+		</>
+	)
+}
